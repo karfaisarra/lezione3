637a5725dbb3ff0b966ea3150fb6c1c5e652cd89
karfaisarra <karfaisarra@gmail.com>
2023-05-04 20:48:54 +0200

    add exercice

---
+++ OrdineDecrescente.java
@@ -0,0 +1,12 @@
+package lezione3;
+
+public class OrdineDecrescente {
+	public static void main(String args[]) {
+		
+		int i = 30;
+		while(i>=15) {
+			System.out.println(i);
+			i--;
+		}
+	}
+}
